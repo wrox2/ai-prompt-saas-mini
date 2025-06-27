771d700067a12406e1e68e520565f187b28906dd
wrox2 <marius.r.b@hotmail.no>
2025-06-28 00:53:34 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,7 @@
-export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <html lang="en">
-        <body className="bg-gray-50">{children}</body>
-      </html>
-    );
-  }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-50">{children}</body>
+    </html>
+  );
+}
